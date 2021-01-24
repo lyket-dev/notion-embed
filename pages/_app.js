e999fd2d4f4707c88b857de4567055363cbf77d8
Irene Oppo <i.oppo@datocms.com>
2021-01-24 12:47:30 +0100

    Removed log

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import { Provider } from "@lyket/react";
 
 function MyApp({ Component, pageProps }) {
-  console.log(process.env.NEXT_PUBLIC_LYKET_API_KEY);
   return (
     <Provider apiKey={process.env.NEXT_PUBLIC_LYKET_API_KEY}>
       <Component {...pageProps} />
