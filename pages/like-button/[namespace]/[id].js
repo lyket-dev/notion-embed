178b066ba8bdf7c220e2fd14c63c9ed4c95ffefd
vizor132 <93132083+vizor132@users.noreply.github.com>
2021-10-26 14:02:08 +0300

    Update [id].js

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -1,33 +1,24 @@
 import { LikeButton } from '@lyket/react';
-
-export Faq = () => {
-  return (
-    <>
-      <h4 id="Do you like pizza?">Do you like pizza?</h4>
-      <LikeButton
-        id="do-you-like-pizza"
-        namespace="faq"
-        hideCounterIfLessThan={1}
-      >
-        {({
-          handlePress,
-          totalLikes,
-          userLiked,
-          isLoading,
-          isCounterVisible
-        }) => (
-          <>
-            <button onClick={handlePress} disabled={isLoading}>
-              🍕
-            </button>
-            {isCounterVisible && <div>Total: {totalLikes}</div>}
-            {userLiked && <div>Great! I like pizza as well!</div>}
-          </>
-        )}
-      </LikeButton>
-    </>
-  )
-};
+<LikeButton
+  id="pizza"
+  namespace="great-things"
+>
+  {({ handlePress, totalLikes, userLiked, isLoading }) => {
+    return (
+      <div>
+        <button
+          className="big"
+          onClick={handlePress}
+          disabled={isLoading}
+        >
+          🍕
+        </button>
+        <strong>Total likes: {totalLikes}</strong>
+        {userLiked && <div>Thanks for liking pizza!</div>}
+      </div>
+    )
+  }}
+</LikeButton>
 
 import { useRouter } from "next/router";
 
