b7f6a0a0a3c1221e5af5cdbb933769b37bd471ef
vizor132 <93132083+vizor132@users.noreply.github.com>
2022-01-17 21:53:40 +0300

    Update [id].js

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -1,6 +1,18 @@
 import { LikeButton } from "@lyket/react";
 import { useRouter } from "next/router";
 
+<Provider
+  apiKey="st_37c8c8b56b1a1efadfad5978811009"
+  theme={{
+    colors: {
+      background: "rgba(255, 224, 138, 0.4)",
+      text: "violet",
+      primary: "#b8fff3"
+      secondary: "rgba(220, 234, 108, 0.6)"
+    }
+  }}
+>
+
 export default function Button() {
   const router = useRouter();
   const { namespace, id } = router.query;
